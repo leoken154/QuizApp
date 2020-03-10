8edc5ecbd4da7de8fcd7ed6de4af44ffb5ec2875
leoken154 <Kenneth_ings@yahoo.com>
2020-03-09 21:30:26 -0400

    correct css and added addtional filess

---
+++ QuizApp/quiz app files/QuizApp questions.js
@@ -0,0 +1,136 @@
+Q 1 - Which of the following type of variable takes precedence over other if names are same?
+
+A - global variable
+
+B - local variable
+
+C - Both of the above.
+
+D - None of the above.
+
+Answer : local variable
+Explaination
+A local variable takes precedence over a global variable with the same name.
+
+
+Q 2 - Which built-in method returns the calling string value converted to lower case?
+
+A - toLowerCase()
+
+B - toLower()
+
+C - changeCase(case)
+
+D - None of the above.
+
+Answer : A
+Explaination
+toLowerCase() method returns the calling string value converted to lower case.
+
+
+Q 3 - Which of the following jQuery method adds the specified class if it is not present, remove the specified class if it is present?
+
+A - toggleStyleClass( class )
+
+B - toggleClass( class )
+
+C - toggleCSSClass( class )
+
+D - None of the above.
+
+Answer : B
+Explaination
+The toggleClass(class) method adds the specified class if it is not present, removes the specified class if it is present.
+
+
+
+
+4 Which of the following jQuery method sets the text of an element?
+
+A - text( val )
+
+B - setText( val )
+
+C - setContent( val )
+
+D - None of the above.
+
+Answer : A
+Explaination
+The text( val ) sets the text contents of all matched elements.
+
+
+
+
+5 Which of the following jQuery method gets a set of elements containing the closest parent element that matches the specified selector, the starting element included?
+
+A - getNearest( selector )
+
+B - getClosest( [selector])
+
+C - closest(selector)
+
+D - None of the above.
+
+Answer : C
+Explaination
+The closest( selector ) method gets a set of elements containing the closest parent element that matches the specified selector, the starting element included.
+
+
+
+Query - Online Quiz
+Advertisements
+
+
+
+
+6 - Which of the following jQuery method gets the style property of an element?
+
+A - getClass( name )
+
+B - getStyle( name)
+
+C - css( name )
+
+D - None of the above.
+
+Answer : C
+Explaination
+The css( name ) method returns a style property on the first matched element.
+
+
+7 Which of the following is correct about jQuery?
+
+A - jQuery is a fast and concise JavaScript Library created by John Resig in 2006 with a nice motto - Write less, do more.
+
+B - jQuery simplifies HTML document traversing, event handling, animating, and Ajax interactions for rapid web development.
+
+C - jQuery is a JavaScript toolkit designed to simplify various tasks by writing less code.
+
+D - All of the above.
+
+Answer : D
+Explaination
+All of the above options are correct.
+
+
+
+
+
+8 - Which of the following jQuery method loads a remote page using an HTTP request?
+
+A - jQuery.ajax( options )
+
+B - jQuery.ajaxSetup( options )
+
+C - serialize( )
+
+D - serializeArray( )
+
+Answer : A
+Explaination
+The jQuery.ajax( options ) method loads a remote page using an HTTP request.
+
+
+
+
